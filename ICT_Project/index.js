b53add8fe0557da9aba65515b57ebab0f4238838
MadFury82 <70714423+MadFury82@users.noreply.github.com>
2020-11-22 18:41:07 +0600

    Add files via upload

---
+++ ICT_Project/index.js
@@ -0,0 +1,10 @@
+$(".di").on("click",function(){
+  const className=this.name;
+  console.log(className);
+  $(".sheduleImg").attr("src", "shedules/"+className+".png" );
+});
+
+
+$(".biologyClass").on("click",function(){
+console.log(event);
+});
